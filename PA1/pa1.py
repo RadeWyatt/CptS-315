8a5aa2dbaadc5c2fc056abc2ba1c9880a2cd2c12
Rade <rade.pilkinton@wsu.edu>
2020-09-26 17:17:47 -0700

    done'

--- PA1/pa1.py
+++ PA1/pa1.py
@@ -2,108 +2,141 @@ from apyori import apriori
 import pandas as pd
 import csv
 
-support = 100
-baskets = []
-with open("browsing-data.txt", "r") as a_file:
-  for line in a_file:
-      baskets.append(line.split(' ')[:-1])
-#print(baskets)
+def insert(list, sub): 
+    # Searching for the position 
+    i = 0
+    length = len(list)
+    while i < length:
+        if float(list[i][2]) > sub[2]: 
+            break
+        i+=1
+    # Inserting n in the list 
+    list = list[:i] + [sub] + list[i:] 
+    return list
 
-index = 0
-temp = 0
+def main():
+    support = 100
+    baskets = []
+    with open("browsing-data.txt", "r") as a_file:
+        for line in a_file:
+            baskets.append(line.split(' ')[:-1])
+    #print(baskets)
 
-with open('browsing-data.txt', 'r') as csvfile:
-    hw1reader = csv.reader(csvfile, delimiter = ' ')
-    for row in hw1reader:
-        new_str = ''
-        #print('Length: ', len(row))
-        if index == 0:
-            temp = len(row)
-        elif len(row) > temp:
-            temp = len(row)
-        index = index + 1
-        #print('Index: ', index)
+    index = 0
+    temp = 0
+
+    with open('browsing-data.txt', 'r') as csvfile:
+        hw1reader = csv.reader(csvfile, delimiter = ' ')
+        for row in hw1reader:
+            new_str = ''
+            #print('Length: ', len(row))
+            if index == 0:
+                temp = len(row)
+            elif len(row) > temp:
+                temp = len(row)
+            index = index + 1
+            #print('Index: ', index)
+            
+            for x in range(len(row)):
+                if(row[x] != ''):
+                    new_str += row[x]
+                    new_str += ', '
+            #print(new_str)
+            #print("***************************")
+            #print("***************************")
+
+    print('Maximum Length: ', temp)
+
+    maxLength = temp
+
+    index = 0
+    temp = 0
+
+    new_file = ''
+
+    with open('browsing-data.txt', 'r') as csvfile:
+        hw1reader = csv.reader(csvfile, delimiter = ' ')
+        for row in hw1reader:
+            new_str = ''
+            #print('Length: ', len(row))
+            index += 1
+            #print('Index: ', index)
+            
+            for x in range(len(row)):
+                if(row[x] != ''):
+                    new_str += row[x]
+                    new_str += ', '
+            
+            #print(new_str)
+            
+            if(len(row) < maxLength):
+                diff = maxLength - len(row)
+                for x in range(len(row), maxLength):
+                    new_str += ', '
+                    
+            #print(new_str)
+            new_file = new_file + '\n' + new_str + '\n'
+            
+            #print("***************************")
+            #print("***************************")
+            
+    print(maxLength)
+    #print(new_file)
+
+    with open("PA1a.txt", "w+") as f:
+        f.write(new_file)
+            
+    data = pd.read_csv('PA1a.txt', header = None)
+    data.head()
+
+    data.dropna()
+    data.head()
+    data.info()
+
+    records = []
+    rows = data.shape[0]
+    cols = data.shape[1]
+    print(rows)
+    print(cols)
+
+    for i in range(0, rows):
+        records.append([str(data.values[i,j]) for j in range(0,cols)])
+    sup = 100 / rows
         
-        for x in range(len(row)):
-            if(row[x] != ''):
-                new_str += row[x]
-                new_str += ', '
-        #print(new_str)
-        #print("***************************")
-        #print("***************************")
+    hw1rules = apriori(records, min_support=sup, min_confidence=0.4, min_lift=3, min_length=2)
+    hw1results = list(hw1rules)
 
-print('Maximum Length: ', temp)
+    print(len(hw1results))
 
-maxLength = temp
+    pairs = []
+    triplets = []
 
-index = 0
-temp = 0
-
-new_file = ''
-
-with open('browsing-data.txt', 'r') as csvfile:
-    hw1reader = csv.reader(csvfile, delimiter = ' ')
-    for row in hw1reader:
-        new_str = ''
-        #print('Length: ', len(row))
-        index += 1
-        #print('Index: ', index)
-        
-        for x in range(len(row)):
-            if(row[x] != ''):
-                new_str += row[x]
-                new_str += ', '
-        
-        #print(new_str)
-        
-        if(len(row) < maxLength):
-            diff = maxLength - len(row)
-            for x in range(len(row), maxLength):
-                new_str += ', '
-                
-        #print(new_str)
-        new_file = new_file + '\n' + new_str + '\n'
-        
-        #print("***************************")
-        #print("***************************")
-        
-print(maxLength)
-print(new_file)
-
-with open("PA1a.txt", "w+") as f:
-    f.write(new_file)
-        
-data = pd.read_csv('PA1a.txt', header = None)
-data.head()
-
-data.dropna()
-data.head()
-data.info()
-
-records = []
-rows = data.shape[0]
-cols = data.shape[1]
-print(rows)
-print(cols)
-
-for i in range(0, rows):
-    records.append([str(data.values[i,j]) for j in range(0,cols)])
-sup = 100 / rows
-    
-hw1rules = apriori(records, min_support=sup, min_confidence=0.4, min_lift=3, min_length=2)
-hw1results = list(hw1rules)
-
-print(len(hw1results))
-
-with open("output.txt", 'w+') as f:
     for item in hw1results:
         
         pair = item[0]
         items = [x for x in pair]
-        f.write("Rule: " + items[0] + " -> " + items[1] + "\n")
-        
-        f.write("Support: " + str(item[1]) + "\n")
-        
-        f.write("Confidence: " + str(item[2][0][2]) + "\n")
-        f.write("Lift: " + str(item[2][0][3]) + "\n")
-        f.write("=================================================\n")
+
+        if (len(items) == 2 and items[0] != ' ' and items[1] != ' '):
+            sub = [str(items[0]), str(items[1]), float(item[2][0][2])]
+            pairs = insert(pairs, sub)
+            
+        if (len(items) == 3 and items[0] != ' ' and items[1] != ' ' and items[2] != ' '):
+            sub = [str(items[0]), str(items[1]), float(item[2][0][2]), str(items[2])]
+            triplets = insert(triplets, sub)
+
+    with open("output.txt", 'w+') as f:
+        l = len(pairs)
+        l2 = len(triplets)
+        i = 1
+        j = 1
+        f.write("OUTPUT A\n")
+        while i < 6:
+            f.write(pairs[l-i][0] + " " + pairs[l-i][1] + " " + str(pairs[l-i][2]) + "\n")
+            i+=1
+        f.write("OUTPUT B\n")
+        while j < 6:
+            f.write(triplets[l2-j][0] + " " + triplets[l2-j][1] + " " + triplets[l2-j][3] + " " + str(triplets[l2-j][2]) + "\n")
+            j+=1
+
+if __name__ == "__main__":
+    main()
---
+++ PA1/readme.txt
@@ -0,0 +1,6 @@
+This was developed in a Windows environment.
+
+To run this code:
+
+(1) Open up a bash shell or terminal in this directory
+(2) Run "python pa1.py"
